00d2bc0512ff511b0416650c56055be63f20f26a
hoang Hoang <hoangmit@yahoo.com>
2014-01-15 13:37:25 +0800

    include cwig_tools

---
+++ cwig/ctools/bedgraph2cwig.cpp
@@ -0,0 +1,75 @@
+#include "cwig/cwig.h"
+#include "utils/str_utils.h"
+#include "mem/file_archive2.h"
+#include "mem/info_archive.h"
+#include "utils/utils.h"
+#include "utils/param.h"
+#include "intarray/sdarray_sml.h"
+
+#include <cstring>
+#include <tuple>
+#include <fstream>
+
+using namespace std;
+using namespace app_ds;
+
+
+
+void testbig() {
+	//string inp = "D:/temp/textBigwig.bed";
+	string inp = "/home/hoang/sam/data1/11N-170bp.bedGraph";
+	//string inp = "/tmp/wg2V3.bedGraph";
+	ifstream fi(inp.c_str());
+	GenomeNumDataBuilder bd;
+	bd.init(true);
+	string lastchr = "";
+	while (fi) {
+		string line;
+		getline(fi,line);
+		if (line.empty()) break;
+		BED_Entry e;
+		e.parse(line);
+		if (e.chrom != lastchr) {
+			//if (lastchr.length() > 0) break;
+			cout << e.chrom << endl;
+			lastchr = e.chrom;
+			bd.changechr(lastchr);
+		}
+		bd.add(e.st, e.ed, e.val);
+	}
+	mscds::OClassInfoArchive fo;
+	bd.build(fo);
+	fo.close();
+	//ofstream fox("/home/hoang/sam/data1/11N-170bp.info.xml");
+	ofstream fox("/tmp/info.xml");
+	fox << fo.printxml() << endl;
+	fox.close();
+
+	/*GenomeNumData gd;
+	bd.build(&gd);
+	ofstream fox("D:/temp/dump_chr1_rlen.txt");
+	gd.__testing_dump_1st_st(fox);
+	fox.close();*/
+
+	fi.close();
+}
+
+void build(const string& input, const string& output) {
+	GenomeNumDataBuilder bd;
+	try {
+		bd.build_bedgraph(input, output);
+	}catch(std::exception& e) {
+		std::cerr << e.what() << endl;
+	}
+}
+
+int main(int argc, const char* argv[]) {	
+	if (argc >= 3) {
+		Config * c = Config::getInst();
+		c->parse(argc, argv);
+		cout << "params: " << endl;
+		c->dump();
+		build(argv[1], argv[2]);
+	}
+	return 0;
+}
---
+++ cwig/ctools/cwig_summary.cpp
@@ -0,0 +1,84 @@
+
+#include <iostream>
+#include "cwig/cwig.h"
+#include "mem/fmap_archive2.h"
+#include "mem/info_archive.h"
+
+using namespace std;
+using namespace app_ds;
+
+
+
+int main(int argc, char* argv[]) {
+	if (argc != 6 && argc != 7) {
+		cerr << "cwig_summary {avg|cov|min|max|lst|map} <file> <chr> <start> <end> <size=1>" << endl;
+		return 1;
+	}
+	mscds::IFileMapArchive2 fi;
+	fi.open_read(argv[2]);
+	
+	unsigned int start = atoi(argv[4]);
+	unsigned int end = atoi(argv[5]);
+	unsigned int sz = 1;
+	if (argc == 7) sz = atoi(argv[6]);
+	
+	GenomeNumData qs;
+	qs.load(fi);
+	
+	int chr = qs.getChrId(argv[3]);
+	if (chr == -1) {
+		cerr << "cannot find chromosome" << endl;
+		return 1;
+	}
+	string cmd(argv[1]);
+	auto arr = qs.getChr(chr);
+	if (cmd == "avg") {
+		auto arr = qs.getChr(chr).avg_batch(start, end, sz);
+		for (auto it = arr.begin(); it != arr.end(); ++it)
+			cout << *it << "  ";
+		cout << endl;
+	} else
+	if (cmd == "cov") {
+		auto arr = qs.getChr(chr).coverage_batch(start, end, sz);
+		for (auto it = arr.begin(); it != arr.end(); ++it)
+			cout << *it << "  ";
+		cout << endl;
+	} else
+	if (cmd == "min") {
+		auto arr = qs.getChr(chr).min_value_batch(start, end, sz);
+		for (auto it = arr.begin(); it != arr.end(); ++it)
+			cout << *it << "  ";
+		cout << endl;
+	} else
+	if (cmd == "max") {
+		auto arr = qs.getChr(chr).max_value_batch(start, end, sz);
+		for (auto it = arr.begin(); it != arr.end(); ++it)
+			cout << *it << "  ";
+		cout << endl;
+	} else
+	if (cmd == "lst") {
+		auto & chrds = qs.getChr(chr);
+		auto rng = chrds.find_intervals(start, end);
+		cout << "Indexes: " << rng.first << " " << rng.second << endl;
+		if (rng.first < rng.second) {
+			app_ds::ChrNumValType::Enum e;
+			chrds.getEnum(rng.first, &e);
+			for (unsigned int i = rng.first; i < rng.second; ++i) {
+				auto intv = e.next();
+				cout << argv[3] << "\t" << intv.st << "\t" << intv.ed << "\t" << intv.val << "\n";
+			}
+			cout << endl;
+		}
+	} else
+	if (cmd == "map") {
+		auto & chrds = qs.getChr(chr);
+		auto arr = chrds.base_value_map(start, end);
+		for (unsigned int i = 0; i < arr.size(); ++i)
+			cout << arr[i] << " ";
+		cout << endl;
+	} else {
+		cerr << "unknow query" << endl;
+	}
+	qs.clear();
+	return 0;
+}

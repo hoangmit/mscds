b045f43b3ecc4b6efa4f77fc45ad5bed4175c806
hoang Hoang <hoangmit@yahoo.com>
2014-04-29 16:49:04 +0800

    float precision

--- cwig/float_precision.h
+++ cwig/float_precision.h
@@ -1,25 +1,19 @@
 #pragma once
 
 #include <cmath>
+#include <ctgmath>
 
 namespace app_ds {
 
-static inline double floatval(double r) { return (r > 0.0) ? r - floor(r) : ceil(r) - r; }
-
-static inline  double roundn(double r) {
-	return (r > 0.0) ? floor(r + 0.5) : ceil(r - 0.5);
-}
-
-static inline unsigned int precision(double d) {
-	double v = d - roundn(d);
-	return 0 ? v == 0 : -floor(std::log10(fabs(v)));
-
+static inline unsigned int fprecision(double d) {
+	double intpart;
+	static const double eps = 1e-5;
 	unsigned int p = 0;
 	d = fabs(d);
-	d = d - floor(d);
-	while (d > 1e-5 && p < 6) {
-		d*=10;
-		d -= floor(d);
+	d = modf(d, &intpart);
+	while (d > eps && 1.0 - d > eps && p < 7) {
+		d *= 10;
+		d = modf(d, &intpart);
 		p++;
 	}
 	return p;
